515ee11575c7d27898ec3a503befb3537062db2d
jota santos <gestald118@email.com>
2024-03-20 22:32:25 -0400

    implementando o store

--- src/app/components/user/create/create.component.ts
+++ src/app/components/user/create/create.component.ts
@@ -4,14 +4,43 @@ import { Component } from '@angular/core';
 import { CardHeaderComponent } from '../../layout/card/card-header/card-header.component';
 import { CardBodyComponent } from '../../layout/card/card-body/card-body.component';
 import { CardFooterComponent } from '../../layout/card/card-footer/card-footer.component';
+import { FormBuilder, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { UserServiceService } from '../../../services/user-service.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create',
   standalone: true,
-  imports: [CardHeaderComponent, CardBodyComponent, CardFooterComponent],
+  imports: [
+    CardHeaderComponent, CardBodyComponent, CardFooterComponent, ReactiveFormsModule,
+    CommonModule
+  ],
   templateUrl: './create.component.html',
   styleUrl: './create.component.css'
 })
 export class CreateComponent {
+  userForm: FormGroup;
+
+  constructor(
+    private userService: UserServiceService,
+    private router: Router
+    ) {
+    this.userForm = new FormGroup({
+      name: new FormControl("", [Validators.required, Validators.minLength(5), Validators.maxLength(50)]),
+      cpf: new FormControl("", [Validators.required, Validators.minLength(11), Validators.maxLength(11)]),
+      email: new FormControl("", [Validators.required, Validators.minLength(5), Validators.maxLength(50), Validators.email]),
+    });
+  }
+
+  store() {
+    this.userService
+    .store(this.userForm.value)
+    .subscribe(res => {
+      console.log("user salvo com sucesso");
+      this.router.navigateByUrl('user/index');
+    });
+  }
+
 
 }
--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -3,51 +3,48 @@ import { Observable, catchError, throwError } from 'rxjs';
 import { User } from '../interfaces/user';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { ApiPath } from '../shared/apiPath';
+import { httpOptions } from '../config/http-options';
 
 @Injectable({
   providedIn: 'root',
 })
 export class UserServiceService {
-
-  constructor(
-    private httpClient: HttpClient
-  ) {}
-
-  httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json'
-    })
- }
+  constructor(private httpClient: HttpClient) {}
 
   getListLocalUsers() {
     return [
       {
         id: 1,
-        name: "Jota Santos",
-        cpf: "046.667.061-35",
-        email: "gestald118@gmail.com",
+        name: 'Jota Santos',
+        cpf: '046.667.061-35',
+        email: 'gestald118@gmail.com',
         ativo: true,
-        created_at: "2024-05-09",
-      },    
-
+        created_at: '2024-05-09',
+      },
     ];
   }
 
   getUsers(): Observable<User[]> {
-    return this.httpClient.get<User[]>(ApiPath.USERS).pipe(
-      catchError(this.errorHandler)
-    )
+    return this.httpClient
+      .get<User[]>(ApiPath.USERS)
+      .pipe(catchError(this.errorHandler));
+  }
+
+  store(user: User): Observable<User> {
+    return this.httpClient
+      .post<User>(ApiPath.USERS, JSON.stringify(user), httpOptions)
+      .pipe(catchError(this.errorHandler));
   }
 
   errorHandler(error: any): Observable<never> {
     let errorMessage = '';
-    if(error.error instanceof ErrorEvent) {
+    if (error.error instanceof ErrorEvent) {
       errorMessage = error.error.message;
     } else {
       errorMessage = `Error Code: ${error.status}\nMessage: ${error.message}`;
     }
     console.log(error);
-    
+
     throw new Error(errorMessage);
   }
 }
